50c7f685813e6d901b087bd120b2844643f4f720
Grant Skinner <info@gskinner.com>
2019-03-15 14:01:31 -0600

    Initial hacky build system for darkUI

--- gulpfile.js
+++ gulpfile.js
@@ -24,8 +24,8 @@ var buildVersion = null;
 gulp.task("default", ["browser-sync", "build-default", "watch"]);
 gulp.task("build-default", ["build-js", "build-sass"], browserSync.reload);
 
-gulp.task("build", function(cb) {
-	runSequence(["build-js-prod", "build-sass"], function() {
+gulp.task("build-deploy", function(cb) {
+	runSequence(["build-js-prod", "build-sass-dark"], function() {
 		browserSync.reload();
 		cb();
 	});
@@ -33,7 +33,7 @@ gulp.task("build", function(cb) {
 
 gulp.task("deploy", function(cb) {
 	buildVersion = getBuildVersion();
-	runSequence("build", "clean-build", "copy-build", "inject-file-versions", "inject-build-version", "minify-html", cb);
+	runSequence("build-deploy", "clean-build", "copy-build", "inject-file-versions", "inject-build-version", "minify-html", cb);
 });
 
 gulp.task("browser-sync", function() {
@@ -74,7 +74,7 @@ gulp.task("build-js", function() {
 });
 
 gulp.task("build-js-prod", function() {
-	return 	rollup({
+	return rollup({
 			entry: "dev/src/app.js",
 			moduleContext: {"dev/lib/codemirror.js":"window"},
 			plugins: [babel({
@@ -142,7 +142,6 @@ gulp.task("inject-build-version", function(cb) {
 	fs.writeFile("build/deploy/regexr.js", js, cb);
 });
 
-
 gulp.task("minify-html", function() {
 	return gulp.src("build/index.html")
 		.pipe(htmlmin({collapseWhitespace: true, conservativeCollapse: true, removeComments: true}))
@@ -202,4 +201,114 @@ function getDateString() {
 	var now = new Date();
 	var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	return months[now.getMonth()]+" "+now.getDate()+", "+now.getFullYear();
+}
+
+// dark theme:
+// TODO: this whole approach can be cleaned up, ideally when moving to gulp v4
+var dark = "", root = "";
+gulp.task("build-sass-dark0", function(cb) {
+	root = fs.readFileSync("dev/sass/regexr.scss", "utf-8");
+	fs.writeFile("dev/sass/regexr.scss", root.replace('@import "colors";', '@import "colors_dark";'), cb);
+});
+gulp.task("build-sass-dark1", function(cb) {
+	dark = fs.readFileSync("deploy/regexr.css", "utf-8");
+	fs.writeFile("dev/sass/regexr.scss", root, cb);
+});
+gulp.task("build-sass-dark2", function(cb) {
+	var def = fs.readFileSync("deploy/regexr.css", "utf-8");
+	var diff = (new CSSDiff()).diff(def, dark, false);
+	fs.writeFile("assets/themes/dark.css", diff, cb);
+	dark = root = ""; // clean up.
+});
+
+gulp.task("build-sass-dark", function(cb) {
+	runSequence("build-sass-dark0", "build-sass", "build-sass-dark1", "build-sass", "build-sass-dark2", cb);
+});
+
+
+// CSSDiff:
+class CSSDiff {
+	constructor() {}
+
+	diff(base, targ, pretty=false) {
+		let diff = this.compare(this.parse(base), this.parse(targ));
+		return this._writeDiff(diff, pretty);
+	}
+
+	parse(s, o={}) {
+		this._parse(s, /([^\n\r\{\}]+?)\s*\{\s*/g, /\}/g, o);
+		for (let n in o) {
+			if (n === " keys") { continue; }
+			o[n] = this.parseBlock(o[n]);
+		}
+		return o;
+	}
+
+	parseBlock(s, o={}) {
+		return this._parse(s, /([^\s:]+)\s*:/g, /(?:;|$)/g, o);
+	}
+
+	compare(o0, o1, o={}) {
+		let keys = o1[" keys"], l=keys.length, arr=[];
+		for (let i=0; i<l; i++) {
+			let n = keys[i];
+			if (!o0[n]) { o[n] = o1[n]; arr.push(n); continue; }
+			let diff = this._compareBlock(o0[n], o1[n]);
+			if (diff) { o[n] = diff; arr.push(n); }
+		}
+		o[" keys"] = arr;
+		return o;
+	}
+
+	_compareBlock(o0, o1) {
+		let keys = o1[" keys"], l=keys.length, arr=[], o;
+		for (let i=0; i<l; i++) {
+			let n = keys[i];
+			if (o0[n] === o1[n]) { continue; }
+			if (!o) { o = {}; }
+			o[n] = o1[n];
+			arr.push(n);
+		}
+		if (o) { o[" keys"] = arr; }
+		return o;
+	}
+
+	_parse(s, keyRE, closeRE, o) {
+		let i, match, arr=[];
+		while (match = keyRE.exec(s)) {
+			let key = match[1];
+			i = closeRE.lastIndex = keyRE.lastIndex;
+			if (!(match = closeRE.exec(s))) { console.log("couldn't find close", key); break; }
+			o[key] = s.substring(i, closeRE.lastIndex-match[0].length).trim();
+			i = keyRE.lastIndex = closeRE.lastIndex;
+			arr.push(key);
+		}
+		o[" keys"] = arr;
+		return o;
+	}
+
+	_writeDiff(o, pretty=false) {
+		let diff = "", ln="\n", s=" ";
+		if (!pretty) { ln = s = ""; }
+		let keys = o[" keys"], l=keys.length;
+		for (let i=0; i<l; i++) {
+			let n = keys[i];
+			if (diff) { diff += ln + ln; }
+			diff += n + s + "{" + ln;
+			diff += this._writeBlock(o[n], pretty);
+			diff += "}";
+		}
+		return diff;
+	}
+
+	_writeBlock(o, pretty=false) {
+		let diff = "", ln="\n", t="\t", s=" ";
+		if (!pretty) { ln = t = s = ""; }
+		let keys = o[" keys"], l=keys.length;
+		for (let i=0; i<l; i++) {
+			let n = keys[i];
+			diff += t + n + ":" + s + o[n] + ";" + ln;
+		}
+		return diff;
+	}
 }
